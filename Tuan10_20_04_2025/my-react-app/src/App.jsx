1054f729225efa498a8e1711b5f365d6d8eb4e70
haphu2512-java <haxuanphudeptrai123@gmail.com>
2025-04-21 15:29:00 +0700

    1.CounterApp

--- Tuan10_20_04_2025/my-react-app/src/App.jsx
+++ Tuan10_20_04_2025/my-react-app/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import CounterUseReducer from './components/CounterUseReducer';
-
-import './App.css'; 
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/counterSlice';
 
 function App() {
-  return (
-    
-        <CounterUseReducer />
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch(increment())} style={{ marginRight: '10px' }}>
+        Tăng
+      </button>
+      <button onClick={() => dispatch(decrement())}>
+        Giảm
+      </button>
+    </div>
   );
 }
 
